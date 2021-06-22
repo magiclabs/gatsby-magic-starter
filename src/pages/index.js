683f28d139dee06b820702da289ab236fa0bafc7
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-06-23 01:55:44 +0530

    Magic added to Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,42 @@
-import * as React from "react"
+import React, {useState, useEffect} from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+import { magic } from "../lib/magic";
+
+const IndexPage = () => { 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // On mount, we check if a user is logged in.
+    // If so, we'll retrieve the authenticated user's profile.
+    magic.user.isLoggedIn().then(setIsLoggedIn);
+  }, []);
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site with passwordless authentication powered by <a href="https://magic.link" target="_blank" rel="noopener noreferrer">Magic</a>.</p>
+
+      <p>
+        {isLoggedIn ? <Link to="/app/profile">Profile</Link> : <Link to="/app/login">Login</Link>}
+      </p>
+
+      <StaticImage
+        src="../images/gatsby-magic.png"
+        width={500}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Gatsby site with passwordless authentication powered by Magic."
+        style={{ marginBottom: `1.45rem` }}
+      />
+      
+    </Layout>
+  )
+}
 
 export default IndexPage
