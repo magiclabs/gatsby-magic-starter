ba534d5a32fc5d32003dfd53440be27f4e3ed971
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-06-23 01:37:13 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Magic Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@magic_labs @mdsbzalam`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
